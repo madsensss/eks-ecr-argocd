3b9899712458ebc473e3ddb86dc01ad7f2da6c7f
madsensss <madsenss@naver.com>
2025-02-04 10:07:41 +0900

    Update StudentController.java

--- src/main/java/student/controller/StudentController.java
+++ src/main/java/student/controller/StudentController.java
@@ -26,7 +26,7 @@ public class StudentController {
     
     @GetMapping("/hello")
     public String sayHello() {
-        return "hello PRIVATE RDS2 !@";
+        return "hello PRIVATE RDS3 !@";
     }
     
     @GetMapping
